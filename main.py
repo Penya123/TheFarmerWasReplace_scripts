35a159276de1e827a67e4b9518ad0863677b120c
Jorge Sandoval <jorgesagt5@gmail.com>
2026-04-20 18:53:08 -0700

    Se añadió un comentario documentando el cambió de lógica

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import getReady
 import NewLogic
 
+# DESCRIPCIÓN DEL FLUJO
+# Primero se plantan girasoles en 
+# todo el campo y se cosechan 3 veces.
+# Después se plantan y se consechan 
+# calavazas 3 veces.
+# Por último se cosecha se la manera clasica:
+# 3 lineas de zanahorias y 9 de arboles y trigo
+#
+# Esto esta sujeto a cambios durante el desarrollo
 clear()
 carrotRows = 3
 treeRows = 9
